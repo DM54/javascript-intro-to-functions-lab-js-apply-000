d89df140f9548f560d5fc9049762cf5ad4b80ed4
Dyana Monroy <dyana.monroy17@live.com>
2019-06-16 23:14:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function sayHiToGrandma(string){
     return "I can\'t hear you!";
   }
 
-  else if (string.toUpperCase() === uppercase){
+  else if (string.toUpperCase() === lowercase){
     return "YES INDEED!";
     
     }
